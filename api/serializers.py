33b9a24e4af006be55e181639b78dceae8f0db13
Ahmed-I-Abdullah <70468508+Ahmed-I-Abdullah@users.noreply.github.com>
2021-05-29 14:35:34 +0200

    feat: add contacts serializer and view

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,13 @@
 from django.db.models import fields
 from rest_framework import serializers
-from meetings.models import Meeting
+from meetings.models import Meeting, Contact
 
 class MeetingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Meeting
         fields = ('id', 'name', 'link', 'start_date_time', 'message', 'notified_contacts')
+
+class ContactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contact      
+        fields = ('id', 'first_name', 'last_name', 'phone_number')
--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import MeetingList
+from .views import MeetingList, ContactList
 
 app_name = 'api'
 
 urlpatterns = [
-    path('', MeetingList.as_view(), name="listcreate")
+    path('', MeetingList.as_view(), name="meeting_listcreate"),
+    path('contacts', ContactList.as_view(), name="contact_listcreate"),
 ]
