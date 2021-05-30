4f1fc7e34d112e2af40643155673e3e32a64e5eb
Ahmed-I-Abdullah <70468508+Ahmed-I-Abdullah@users.noreply.github.com>
2021-05-30 04:58:50 +0200

    test: api get/post tests

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,47 @@
-from django.test import TestCase
+from django.http import response
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from meetings.models import User, Contact, Meeting
 
-# Create your tests here.
+class ContactsTest(APITestCase):
+    def test_view_contacts(self):
+        end_point = reverse('api:contact_listcreate')
+        response = self.client.get(end_point, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_contacts(self):
+        end_point = reverse('api:contact_listcreate')
+        contact = {
+            'first_name': 'test', 
+            'last_name': 'contact', 
+            'phone_number': '+16135550141'
+            }    
+        response = self.client.post(end_point, contact, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)    
+
+class MeetingsTest(APITestCase):
+    def test_view_meetings(self):
+        end_point = reverse('api:meeting_listcreate')
+        response = self.client.get(end_point, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_create_meetings(self):
+        end_point = reverse('api:meeting_listcreate')
+        test_contact = Contact.objects.create(
+            first_name = 'test', 
+            last_name = 'contact', 
+            phone_number = '+16135550141' # fake phone number from https://fakenumber.org/canada
+            )
+        meeting = {
+            'name': 'daily standup',
+            'link': 'https://example.com/brake/boundary.html',
+            'start_date_time': '2021-05-21T04:57:00Z',
+            'message': 'Hi, I have a meeting in a few minutes',
+            'notified_contacts': [
+                1
+            ]
+         }  
+        response = self.client.post(end_point, meeting, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)            
+        
