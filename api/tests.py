a0b26b6654dea32955a8dc8e4659c803c8daa07c
Ahmed-I-Abdullah <70468508+Ahmed-I-Abdullah@users.noreply.github.com>
2021-06-12 18:50:12 +0200

    test: update tests for new models and permissions

--- api/tests.py
+++ api/tests.py
@@ -3,37 +3,66 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 from meetings.models import Contact
+from django.contrib.auth.models import User
+
 
 class ContactsTest(APITestCase):
     def test_view_contacts(self):
         end_point = reverse('api:contact_listcreate')
+        test_user = User.objects.create_user(
+            username='test', password='12345678910')
+
+        self.client.login(username=test_user.username,
+                          password='12345678910')
         response = self.client.get(end_point, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_create_contacts(self):
         end_point = reverse('api:contact_listcreate')
+        test_user = User.objects.create_user(
+            username='test', password='12345678910')
+
+        self.client.login(username=test_user.username,
+                          password='12345678910')
         contact = {
-            'first_name': 'test', 
-            'last_name': 'contact', 
-            'phone_number': '+16135550141'
-            }    
+            'first_name': 'test',
+            'last_name': 'contact',
+            'phone_number': '+16135550141',
+            'associated_user': 1
+        }
+
         response = self.client.post(end_point, contact, format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)    
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
 
 class MeetingsTest(APITestCase):
     def test_view_meetings(self):
         end_point = reverse('api:meeting_listcreate')
+
+        test_user = User.objects.create_user(
+            username='test', password='12345678910')
+
+        self.client.login(username=test_user.username,
+                          password='12345678910')
         response = self.client.get(end_point, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_create_meetings(self):
         end_point = reverse('api:meeting_listcreate')
-        test_contact = Contact.objects.create(
-            first_name = 'test', 
-            last_name = 'contact', 
-            phone_number = '+16135550141' # fake phone number from https://fakenumber.org/canada
-            )
+        test_user = User.objects.create_user(
+            username='test', password='12345678910')
+
+        self.client.login(username=test_user.username,
+                          password='12345678910')
+        Contact.objects.create(
+            first_name='test',
+            last_name='contact',
+            phone_number='+16135550141',  # fake phone number from https://fakenumber.org/canada
+            associated_user=test_user
+        )
+
         meeting = {
+            'user': 1,
             'name': 'daily standup',
             'link': 'https://example.com/brake/boundary.html',
             'start_date_time': '2021-05-21T04:57:00Z',
@@ -41,6 +70,6 @@ class MeetingsTest(APITestCase):
             'notified_contacts': [
                 1
             ]
-         }  
+        }
         response = self.client.post(end_point, meeting, format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)            
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
--- meetings/tests.py
+++ meetings/tests.py
@@ -1,64 +1,74 @@
 from django.test import TestCase
 from meetings.models import User, Meeting, Contact
 import datetime
-from django.core.exceptions import ValidationError
+
 
 class TestCreateContact(TestCase):
     @classmethod
     def setUpTestData(cls):
-        test_contact = Contact.objects.create(
-            first_name = 'test', 
-            last_name = 'contact', 
-            phone_number = '+16135550141' # fake phone number from https://fakenumber.org/canada
-            )
+        test_user = User.objects.create_user(
+            username='test', password='12345678910')
+
+        Contact.objects.create(
+            first_name='test',
+            last_name='contact',
+            phone_number='+16135550141',  # fake phone number from https://fakenumber.org/canada
+            associated_user=test_user
+        )
 
     def test_contact_content(self):
         contact = Contact.objects.get(id=1)
         first_name = f'{contact.first_name}'
         last_name = f'{contact.last_name}'
         phone_number = f'{contact.phone_number}'
+        associated_user = contact.associated_user
         self.assertEqual(first_name, 'test')
         self.assertEqual(last_name, 'contact')
         self.assertEqual(phone_number, '+16135550141')
+        self.assertEqual(associated_user, User.objects.get(id=1))
 
     def test_str(self):
         contact = Contact.objects.get(id=1)
-        self.assertEqual(str(contact), 'Contact: test, +16135550141')      
+        self.assertEqual(str(contact), 'Contact: test, +16135550141')
+
 
 class TestCreateMeeting(TestCase):
     @classmethod
     def setUpTestData(cls):
+        test_user = User.objects.create_user(
+            username='test', password='12345678910')
+
         test_contact = Contact.objects.create(
-            first_name = 'test', 
-            last_name = 'contact', 
-            phone_number = '+16135550141' # fake phone number from https://fakenumber.org/canada
-            )
-
-        date_time_obj = datetime.datetime(2021, 11, 28, 23, 55, 59, 342380)   
-
-        test_meeting  = Meeting.objects.create(
-            name = 'refinement meeting',
-            link = 'http://example.com/bike',
-            start_date_time =  date_time_obj,
+            first_name='test',
+            last_name='contact',
+            phone_number='+16135550141',  # fake phone number from https://fakenumber.org/canada
+            associated_user=test_user
         )
-        test_meeting.notified_contacts.add(test_contact)    
 
-    def test_contact_content(self):
+        date_time_obj = datetime.datetime(2021, 11, 28, 23, 55, 59, 342380)
+
+        test_meeting = Meeting.objects.create(
+            user=test_user,
+            name='refinement meeting',
+            link='http://example.com/bike',
+            start_date_time=date_time_obj,
+        )
+        test_meeting.notified_contacts.add(test_contact)
+
+    def test_meeting_content(self):
         meeting = Meeting.objects.get(id=1)
+        user = meeting.user
         name = f'{meeting.name}'
         link = f'{meeting.link}'
         start_date_time = f'{meeting.start_date_time}'
         notified_contact = meeting.notified_contacts.all()[0]
-        self.assertNotEqual(notified_contact, 'Contact: test, +16135550141')
+        self.assertEqual(notified_contact, Contact.objects.get(id=1))
+        self.assertEqual(user, User.objects.get(id=1))
         self.assertEqual(name, 'refinement meeting')
         self.assertEqual(link, 'http://example.com/bike')
         self.assertEqual(start_date_time, '2021-11-28 23:55:59.342380+00:00')
 
     def test_str(self):
         meeting = Meeting.objects.get(id=1)
-        self.assertEqual(str(meeting), 'Meeting: refinement meeting, 2021-11-28 23:55:59.342380+00:00')   
-
-class TestCreateUser(TestCase):
-    def test_phone_exception(self):
-      test_user = User.objects.create(username='test_use1', password='12345678')
-      self.assertRaises(ValidationError, msg='Phone number is required.')   
+        self.assertEqual(
+            str(meeting), 'Meeting: refinement meeting, 2021-11-28 23:55:59.342380+00:00')
